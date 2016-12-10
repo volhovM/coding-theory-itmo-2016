be6b6d084c32173a52a2017ac67c89aea05ae520
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-12-10 11:18:47 +0300

    Finish huffman

--- src/Archivers.hs
+++ src/Archivers.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ViewPatterns     #-}
+
 -- | Homework 3
 
 module Archivers where
@@ -9,12 +11,17 @@ import           Control.Monad.Writer  (MonadWriter, Writer, runWriter, tell)
 import           Data.Bifunctor        (second)
 import           Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as BS
+import           Data.List             (dropWhileEnd)
 import qualified Data.Map.Strict       as M
+import           Data.Ord              (comparing)
 import           Data.Ratio            ((%))
 import qualified Data.Text             as T
 import           Universum             hiding ((%))
 
 type String = [Char]
+
+dropEnd :: Int -> [a] -> [a]
+dropEnd i xs = take (length xs - i) xs
 
 log2 :: Double -> Double
 log2 k = log k / log 2
@@ -38,8 +45,8 @@ data HuffmanTrace = HuffmanTrace
 instance Show HuffmanTrace where
     show HuffmanTrace {..} =
         intercalate
-            ", "
-            [show hCurChar, show hProb, show hCodeWord, show hMsgLength]
+            "|"
+            [[hCurChar], show hProb, hCodeWord, show hMsgLength]
 
 huffman :: ByteString -> Logger HuffmanTrace ((Map Char (String,Int)),String)
 huffman input = encode 0 []
@@ -57,15 +64,22 @@ huffman input = encode 0 []
     n = BS.length input
     firstPass :: Map Char Int
     firstPass = BS.foldr' (M.alter (pure . maybe 1 (+1))) M.empty input
-    calcWords :: [(Double,[(Char,String)])]
+    calcWords :: [(Double,[(Char,Int)])]
     calcWords =
-        map (\(k, x) -> (fromIntegral x / fromIntegral n,[(k,[])])) $ M.assocs firstPass
+        map (\(k, x) -> (fromIntegral x / fromIntegral n,[(k,0)])) $ M.assocs firstPass
     calcCodeWordDo [(p,x)] = assert (p == 1) x
-    calcCodeWordDo ((p0,lefts):(p1,rights):xs) =
-        let append a = map (second (a:))
-        in calcCodeWordDo $ sort $ (p0 + p1, append '0' lefts ++ append '1' rights):xs
+    calcCodeWordDo (sortOn fst -> ((p0,lefts):(p1,rights):xs)) =
+        let inc = map (second (+1))
+        in calcCodeWordDo $ sortOn fst $ (p0 + p1, inc lefts ++ inc rights):xs
     codeWords :: [(Char, String)]
-    codeWords = calcCodeWordDo $ sort calcWords
+    codeWords = let res = sortOn snd $ calcCodeWordDo calcWords
+                in foldl' codeNext [] res
+    codeNext [] (c,l) = [(c, replicate l '0')]
+    codeNext xs@((_,pr):_) (c,l) =
+        let -- generates next codeword after pr of length l
+            nextWord = let pr' = dropEnd 1 $ dropWhileEnd (== '1') pr
+                       in pr' ++ "1" ++ replicate (l - length pr' - 1) '0'
+        in (c,nextWord):xs
     table1 = M.fromList $ map (\(c,s) -> (c,(s,firstPass M.! c))) codeWords
 
 runHuffman x = do
